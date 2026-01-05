72605badfd7cf8d70113cab7c96e920a92704011
hasnainworktime <hasnain.workday@gmail.com>
2026-01-05 16:08:40 +0500

    Implement functions to load and delete blog posts
    Added loadPosts and deletePost functions to manage blog posts.

--- script.js
+++ script.js
@@ -22,4 +22,33 @@ function savePost() {
   alert("Post Published!");
   window.location.href = "index.html";
 }
+function loadPosts() {
+  const container = document.getElementById("postsContainer");
+  if (!container) return;
+
+  const posts = JSON.parse(localStorage.getItem("posts")) || [];
+  container.innerHTML = "";
+
+  posts.forEach((post, index) => {
+    container.innerHTML += `
+      <div class="blog-card">
+        <h2>
+          <a href="post.html?index=${index}">${post.title}</a>
+        </h2>
+        <p class="excerpt">${post.content.substring(0, 100)}...</p>
+        <p class="meta">By ${post.author} | ${post.category}</p>
+        <button onclick="deletePost(${index})">Delete</button>
+      </div>
+    `;
+  });
+}
+
+function deletePost(index) {
+  let posts = JSON.parse(localStorage.getItem("posts")) || [];
+  posts.splice(index, 1);
+  localStorage.setItem("posts", JSON.stringify(posts));
+  loadPosts();
+}
+
+loadPosts();
 
